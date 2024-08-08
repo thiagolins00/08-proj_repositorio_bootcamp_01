45bb936cf044c69271913da2ee88c7160d1e53b6
Thiago Lins <thiagolins00@gmail.com>
2024-08-08 00:10:46 -0300

    Salvando meu sexto arquivo

---
+++ main_v2.py
@@ -0,0 +1,10 @@
+#print("Olá, " + input("Digite seu nome: ")+ "!")
+
+numero = 3 #INT
+numero_decimal = 4.56 #FLOAT
+nome_de_usuario = "Thiago Lins" #STRING
+verdadeiro = True #BOOLEAN
+
+print(type(numero))
+
+SENHA_AWS = 11122334555
